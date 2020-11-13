9827c8ce1617d0289911102f89601f61e50f2f93
Hoel Bagard <hoel.bagard@gmail.com>
2020-11-13 20:25:01 +0900

    Uncommented tensorboard metrics

--- utils/crop_codec.sh
+++ utils/crop_codec.sh
@@ -3,6 +3,6 @@
 
 shopt -s globstar
 # Crop and change file format / codec  (for dali)
-for i in **/*.avi; do ffmpeg -i "$i" -filter:v "crop=2300:750:0:900" -pix_fmt yuv420p -b:v 4000k -c:v libx264 "${i%.*}.mp4"; done
+for i in **/*.avi; do ffmpeg -i "$i" -threads 6 -filter:v "crop=2300:750:0:900" -pix_fmt yuv420p -b:v 4000k -c:v libx264 "${i%.*}.mp4"; done
 # Remove the odl avi files
 find . -name "*.avi" -type f -delete
