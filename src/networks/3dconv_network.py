f11ff0355c41d00d7cd850ddc0911b13ab9c76da
Hoel Bagard <hoel.bagard@gmail.com>
2020-12-02 23:45:54 +0900

    Made some progress on 3D conv network (still in progress), added clean exit when killing training process

--- src/networks/3dconv_network.py
+++ src/networks/3dconv_network.py
@@ -1,30 +1,69 @@
+from typing import (
+    Callable,
+    Union,
+    Tuple
+)
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from src.networks.layers import DarknetBlock
+from .network_utils import (
+    layer_init,
+    get_cnn_output_size
+)
 
-from .network_utils import layer_init
-from config.model_config import ModelConfig
+
+class Conv3D(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 3,
+                 stride: Union[int, Tuple[int, int]] = 1, **kwargs):
+        super().__init__()
+
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride,
+                              bias=False, **kwargs)
+        self.batch_norm = nn.BatchNorm3d(out_channels, momentum=0.01)
+
+    def forward(self, input_data: torch.Tensor) -> torch.Tensor:
+        x = self.conv(input_data)
+        x = self.batch_norm(x)
+        x = F.leaky_relu(x, 0.1)
+        return x
 
 
 class Conv3DNet(nn.Module):
-    def __init__(self):
+    def __init__(self, feature_extractor: nn.Module, batch_size: int,
+                 output_classes: int, n_to_n: bool, sequence_length: int,
+                 dim_feedforward: int = 512, nlayers: int = 3,
+                 layer_init: Callable[[nn.Module], None] = layer_init, **kwargs):
+        """
+        CNN feature extractor followed by an transformer layers
+        Args:
+            feature_extractor: CNN to use as a feature extractor
+            batch_size: batch size
+            output_classes: Number of output classes (classification)
+            n_to_n: Whether the model should be N_To_N or N_to_1
+            sequence_length: Length of the input sequence
+            dim_feedforward: Dimension of the fc layers in the transformer encoder part
+            nlayers: Number of sub-encoder-layers in the encoder
+            layer_init: Function used to initialise the layers of the network
+        """
         super().__init__()
-        channels = ModelConfig.CHANNELS
+        self.n_to_n = n_to_n
 
-        self.convs = nn.Sequential(*[DarknetBlock(channels[i-1], channels[i], ModelConfig.NB_BLOCKS[i-1])
-                                     for i in range(1, len(channels))])
-        nn.Conv3d(16, 33, (3, 5, 2), stride=(2, 1, 1), padding=(4, 2, 0))
+        self.feature_extractor = feature_extractor
+        # TODO: Make 3D convs that make sense
+        self.conv3d = Conv3D(16, 33, (3, 5, 2), stride=(2, 1, 1), padding=(4, 2, 0))
 
         self.apply(layer_init)
 
     def forward(self, x):
         batch_size, timesteps, C, H, W = x.size()
 
-        if not ModelConfig.N_TO_N:
-            print("N to 1 not implemented for 3DNet")
+        if self.n_to_n:
+            print("N to N not implemented for 3DNet")
             exit()
         else:
-            print("N to n not implemented for 3DNet")
+            print("N to 1 not implemented for 3DNet")
             exit()
-        return F.log_softmax(x, dim=-1)
+        # x = F.log_softmax(x, dim=-1)
+        return x
--- src/train.py
+++ src/train.py
@@ -10,6 +10,7 @@ from config.data_config import DataConfig
 from src.torch_utils.utils.trainer import Trainer
 from src.torch_utils.utils.tensorboard import TensorBoard
 from src.torch_utils.utils.metrics import Metrics
+from src.torch_utils.utils.ressource_usage import resource_usage
 from src.losses import CE_Loss
 
 
@@ -29,53 +30,63 @@ def train(model: nn.Module, train_dataloader: torch.utils.data.DataLoader, val_d
 
     best_loss = 1000
     last_checkpoint_epoch = 0
+    train_start_time = time.time()
 
-    for epoch in range(ModelConfig.MAX_EPOCHS):
-        epoch_start_time = time.perf_counter()
-        print(f"\nEpoch {epoch}/{ModelConfig.MAX_EPOCHS}")
+    try:
+        for epoch in range(ModelConfig.MAX_EPOCHS):
+            epoch_start_time = time.perf_counter()
+            print(f"\nEpoch {epoch}/{ModelConfig.MAX_EPOCHS}")
 
-        epoch_loss = trainer.train_epoch()
-        if DataConfig.USE_TB:
-            tensorboard.write_loss(epoch, epoch_loss)
-            tensorboard.write_lr(epoch, scheduler.get_last_lr()[0])
+            epoch_loss = trainer.train_epoch()
+            if DataConfig.USE_TB:
+                tensorboard.write_loss(epoch, epoch_loss)
+                tensorboard.write_lr(epoch, scheduler.get_last_lr()[0])
 
-        if (epoch_loss < best_loss and DataConfig.USE_CHECKPOINT and
-                epoch >= DataConfig.RECORD_START and (epoch - last_checkpoint_epoch) >= DataConfig.CHECKPT_SAVE_FREQ):
-            save_path = os.path.join(DataConfig.CHECKPOINT_DIR, f"train_{epoch}.pt")
-            print(f"\nLoss improved from {best_loss:.5e} to {epoch_loss:.5e}, saving model to {save_path}", end='\r')
-            best_loss, last_checkpoint_epoch = epoch_loss, epoch
-            torch.save(model.state_dict(), save_path)
+            if (epoch_loss < best_loss and DataConfig.USE_CHECKPOINT and epoch >= DataConfig.RECORD_START
+                    and (epoch - last_checkpoint_epoch) >= DataConfig.CHECKPT_SAVE_FREQ):
+                save_path = os.path.join(DataConfig.CHECKPOINT_DIR, f"train_{epoch}.pt")
+                print(f"\nLoss improved from {best_loss:.5e} to {epoch_loss:.5e},"
+                      f"saving model to {save_path}", end='\r')
+                best_loss, last_checkpoint_epoch = epoch_loss, epoch
+                torch.save(model.state_dict(), save_path)
 
-        print(f"\nEpoch loss: {epoch_loss:.5e}  -  Took {time.perf_counter() - epoch_start_time:.5f}s")
+            print(f"\nEpoch loss: {epoch_loss:.5e}  -  Took {time.perf_counter() - epoch_start_time:.5f}s")
 
-        # Validation and other metrics
-        if epoch % DataConfig.VAL_FREQ == 0 and epoch >= DataConfig.RECORD_START:
-            with torch.no_grad():
-                validation_start_time = time.perf_counter()
-                epoch_loss = trainer.val_epoch()
+            # Validation and other metrics
+            if epoch % DataConfig.VAL_FREQ == 0 and epoch >= DataConfig.RECORD_START:
+                with torch.no_grad():
+                    validation_start_time = time.perf_counter()
+                    epoch_loss = trainer.val_epoch()
 
-                if DataConfig.USE_TB:
-                    print("\nStarting to compute TensorBoard metrics", end="\r", flush=True)
-                    tensorboard.write_loss(epoch, epoch_loss, mode="Validation")
+                    if DataConfig.USE_TB:
+                        print("\nStarting to compute TensorBoard metrics", end="\r", flush=True)
+                        tensorboard.write_loss(epoch, epoch_loss, mode="Validation")
 
-                    # Metrics for the Train dataset
-                    tensorboard.write_images(epoch, train_dataloader)
-                    if epoch % (3*DataConfig.VAL_FREQ) == 0:
-                        tensorboard.write_videos(epoch, train_dataloader)
-                    train_acc = tensorboard.write_metrics(epoch)
+                        # Metrics for the Train dataset
+                        tensorboard.write_images(epoch, train_dataloader)
+                        if epoch % (3*DataConfig.VAL_FREQ) == 0:
+                            tensorboard.write_videos(epoch, train_dataloader)
+                        train_acc = tensorboard.write_metrics(epoch)
 
-                    # Metrics for the Validation dataset
-                    tensorboard.write_images(epoch, val_dataloader, mode="Validation")
-                    if epoch % (3*DataConfig.VAL_FREQ) == 0:
-                        tensorboard.write_videos(epoch, val_dataloader, mode="Validation")
-                    val_acc = tensorboard.write_metrics(epoch, mode="Validation")
+                        # Metrics for the Validation dataset
+                        tensorboard.write_images(epoch, val_dataloader, mode="Validation")
+                        if epoch % (3*DataConfig.VAL_FREQ) == 0:
+                            tensorboard.write_videos(epoch, val_dataloader, mode="Validation")
+                        val_acc = tensorboard.write_metrics(epoch, mode="Validation")
 
-                    print(f"\nTrain accuracy: {train_acc:.3f}  -  Validation accuracy: {val_acc:.3f}",
-                          end='\r', flush=True)
+                        print(f"Train accuracy: {train_acc:.3f}  -  Validation accuracy: {val_acc:.3f}",
+                              end='\r', flush=True)
 
-                print(f"\nValidation loss: {epoch_loss:.5e}  -"
-                      f"  Took {time.perf_counter() - validation_start_time:.5f}s",
-                      flush=True)
-        scheduler.step()
+                    print(f"\nValidation loss: {epoch_loss:.5e}  -"
+                          f"  Took {time.perf_counter() - validation_start_time:.5f}s",
+                          flush=True)
+            scheduler.step()
+    except KeyboardInterrupt:
+        print("\n")
 
-    print("Finished Training")
+    train_stop_time = time.time()
+    tensorboard.close_writers()
+    memory_peak, gpu_memory = resource_usage()
+    print("Finished Training"
+          f"\n\tTraining time : {train_stop_time - train_start_time:.03f}s"
+          f"\n\tRAM peak : {memory_peak // 1024} MB\n\tVRAM usage : {gpu_memory}")
